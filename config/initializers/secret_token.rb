3fddca3db4c22b745ece8d337735097e70355d3c
Rubén Agudo <ragudol@gmail.com>
2014-03-04 20:26:21 +0100

    primer ejercicio curso ruby aspgems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AspgemsEx1::Application.config.secret_token = '9c5c30fa9ff5ecd562e04727f5a47f51781fa8cc42f2e47adef3d9a207a35ee7c30b4f954439add1341ec299bab636060a95dace38b73ce0bc1956e8ea042cfb'
